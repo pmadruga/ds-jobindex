03a17564730b3b72c8bf5e3741ef96d3649a7b3c
Pedro Madruga <hello@pedromadruga.com>
2020-08-18 20:10:43 +0200

    project scaffolding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data science and machine learning applied on the jobindex.dk dataset',
+    author='pmadruga',
+    license='MIT',
+)
