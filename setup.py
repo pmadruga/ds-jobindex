376ade795546c735a7a34b8e9eed5ebca8709f23
Pedro Madruga <hello@pedromadruga.com>
2020-10-19 09:56:43 +0200

    improved package description

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='jobindex-recommender',
     packages=find_packages(),
     version='0.1.0',
-    description='Data science and machine learning applied on the jobindex.dk dataset',
+    description='Recommender for jobindex.dk archived jobs',
     author='pmadruga',
     license='MIT',
+    python_requires='>=3.6',
 )
